dec3b3951d53ef5583a7bee69c1a4063f5967958
Daniel Hollcraft <daniel@hollcraft.com>
2017-09-13 01:26:46 +0200

    Rewrites dateRange reducer.

--- src/containers/RangeButton/reducer.js
+++ src/containers/RangeButton/reducer.js
@@ -1,30 +1,17 @@
 /**
  * Created by danielhollcraft on 9/8/17.
  */
-import moment from 'moment'
 
 import {
-  WEEK,
-  MONTH,
-  THREE_MONTHS,
-  YEAR
+  ALL,
+  SET_DATE_RANGE
 } from "../App/constants";
 
-export const dateRange = (data = [], dateRange) => {
-
-  const returnEntriesInRange =
-    (units, thresh = 1) => data.filter(e => moment(moment()).diff(e.date, units) < thresh);
-
-  switch(dateRange) {
-    case WEEK:
-      return returnEntriesInRange('weeks');
-    case MONTH:
-      return returnEntriesInRange('months');
-    case THREE_MONTHS:
-      return returnEntriesInRange('months', 3);
-    case YEAR:
-      return returnEntriesInRange('years');
+export const dateRange = (state = ALL, action = {}) => {
+  switch (action.type) {
+    case SET_DATE_RANGE:
+      return action.range;
     default:
-      return data;
+      return state;
   }
 };
--- src/containers/RangeButton/tests/reducer.test.js
+++ src/containers/RangeButton/tests/reducer.test.js
@@ -1,5 +1,11 @@
-describe('test', () => {
-  it('should ', () => {
-    expect(true).toEqual(false)
+import { dateRange } from '../reducer'
+
+describe('dataRange', () => {
+  it('returns a default state of ALL', () => {
+    expect(dateRange()).toEqual('ALL')
+  });
+
+  it('returns inputed action.range', () => {
+    expect(dateRange('ALL', {type:'SET_DATE_RANGE', range: 'WEEK'})).toEqual('WEEK')
   });
 });
